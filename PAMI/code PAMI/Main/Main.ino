533d3c00e6bcd869a9cd0d8357a534277117b7de
Lukas Gye-Jacquot <204527713+luklab63@users.noreply.github.com>
2025-12-20 15:10:45 +0100

    codepami

--- PAMI/code PAMI/Main/Main.ino
+++ PAMI/code PAMI/Main/Main.ino
@@ -3,13 +3,14 @@
 #define wheel2_1 10
 #define wheel2_2 9
 
-#define LED_R  7
-#define LED_G  8
-#define LED_B  13
+#define LED_G  7
+#define LED_B  8
+#define LED_R  12
 
 int pins[]={6,5,10,9,7,8,12};
 
-#define color_switch 11
+#define blue_switch 2
+#define yellow_switch 3
 #define tirette 4
 
 #define time_wait 8500
@@ -23,7 +24,8 @@ void setup() {
     digitalWrite(pins[i],OUTPUT); 
   }
   pinMode(tirette, INPUT);
-  pinMode(color_switch, INPUT);
+  pinMode(blue_switch, INPUT);
+  pinMode(yellow_switch, INPUT);
 
   wait_before();
   wait_go();
